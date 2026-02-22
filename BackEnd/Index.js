3be5fbfc573b3b2cb6e511c137ebd64854e55ced
Pixzzz <122415616+Pixzzz@users.noreply.github.com>
2026-02-22 11:08:45 -0400

    Beginning of Backend implementation

--- BackEnd/Index.js
+++ BackEnd/Index.js
@@ -1,7 +1,10 @@
 const express = require('express')
+const dotenv = require('dotenv')
 const app = express()
 const port = 3000
 
+dotenv.config()
+console.log(process.env)
 app.use(express.json())
 
 app.get('/', (req,res) => {
---
+++ BackEnd/models/BookMark.js
@@ -0,0 +1,22 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const BookMarkSchema = new Schema({
+  Title: { type: String, required: true },
+  Description: { type: String, required: true },
+  URL: { type: String, required: true },
+  UserID: { type: mongoose.Schema.Types.ObjectId, ref: "User", required: true },
+  CreatedAt: { type: Date, default: Date.now },
+  tags: {
+    type: [String],
+    default: [],
+    validate: {
+      validator: function (tags) {
+        return tags.length <= 7;
+      },
+      message: "A bookmark can have a maximum of 7 tags.",
+    },
+  },
+});
+
+model.exports = mongoose.model("BookMark", BookMarkSchema);
